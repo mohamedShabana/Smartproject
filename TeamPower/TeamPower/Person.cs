016c198b10a0a51a7ea0d88273bf957ae4e973af
Mohamed Monir shabana <mohamed_ms2006@hotmail.com>
2017-05-21 09:32:24 +0200

    modify name and add address

--- TeamPower/TeamPower/Person.cs
+++ TeamPower/TeamPower/Person.cs
@@ -8,7 +8,8 @@ namespace TeamPower
 {
     class Person
     {
-        public string name{ get; set;}
+        public string firstName{ get; set;}
+        public string lastName { get; set; }
         public string address { get; set; }
     }
 }
